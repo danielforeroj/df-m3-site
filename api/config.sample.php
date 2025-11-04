a832bf8ebeed186203d2b34f385c3ec4517bb8be
danielforeroj <daniforeroj@gmail.com>
2025-11-03 22:39:50 -0500

    Create config.sample.php

---
+++ api/config.sample.php
@@ -0,0 +1,7 @@
+<?php
+// COPY this file to api/config.php on the server and fill real DB creds there.
+// Do NOT commit api/config.php to git.
+const DB_HOST = 'localhost';
+const DB_NAME = 'YOUR_DB_NAME';
+const DB_USER = 'YOUR_DB_USER';
+const DB_PASS = 'YOUR_DB_PASSWORD';
